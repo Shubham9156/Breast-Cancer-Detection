e6eb2350d9053e56ae03118a232d5288f2b2eb75
Shubham Gade <87603025+Shubham9156@users.noreply.github.com>
2022-03-27 00:11:10 +0530

    Update breast_cancer_detection(logreg).py

--- breast_cancer_detection(logreg).py
+++ breast_cancer_detection(logreg).py
@@ -118,7 +118,7 @@ print(prediction)     # return a list with element [0] if Malignant; returtn a l
 if(prediction[0] == 0):
   print('This breast Cnacer is Malignant')
 else:
-  print('The breast cancer is Belign')
+  print('The breast cancer is Benign')
 
 input_data = (13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259)
 # change the input_data to numpy array to make prediction
@@ -135,5 +135,5 @@ print(prediction)     # return a list with element [0] if Malignant; returtn a l
 if(prediction[0] == 0):
   print('This breast Cnacer is Malignant')
 else:
-  print('The breast cancer is Belign')
+  print('The breast cancer is Benign')
 
